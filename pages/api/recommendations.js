77ec527262f84bb5d5358bbd58fe2f442f051100
cekdarcpr <cekooo12@gmail.com>
2024-05-16 12:41:02 +0300

    update

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -5,7 +5,7 @@ const recommendationCard = [
     name: 'John Minhas',
     image: "images/shahmir.jfif",
     designation: 'React JS | NEXT JS | Node | GraphQL',
-    view: "Hey everyone! I wanted to take a moment to recommend Osama Javaid for any React.js opportunities. I had the pleasure of working with him on multiple projects where he showcased a strong understanding of React.js and Next.js concepts and delivered high-quality code. He consistently demonstrated a willingness to learn and grow, and his enthusiasm for tackling new challenges was contagious. Osama is a reliable and dedicated team member who would be a great asset to any React.js team. Highly recommended! 👍! 🌟",
+    view: "Hey everyone! I wanted to take a moment to recommend Cekdar Capar for any React.js opportunities. I had the pleasure of working with him on multiple projects where he showcased a strong understanding of React.js and Next.js concepts and delivered high-quality code. He consistently demonstrated a willingness to learn and grow, and his enthusiasm for tackling new challenges was contagious. Cekdar is a reliable and dedicated team member who would be a great asset to any React.js team. Highly recommended! 👍! 🌟",
     linkednURL: "/domain-expired"
   },
   {
@@ -13,23 +13,23 @@ const recommendationCard = [
     name: 'Braid Hayat',
     image: "images/sikandar.jpeg",
     designation: 'Data Scientist | Machine Learning Engineer | Python Developer',
-    view: "I had the privilege of working alongside Osama on multiple university projects, and I must say, his talent and work ethic truly stood out. Recently, we were both part of the dynamic team at JMM Technologies, where Osama's professionalism and attention to detail shone brightly. I wholeheartedly endorse Osama for any opportunity that calls for a highly skilled and dedicated individual.",
+    view: "I had the privilege of working alongside Cekdar on multiple university projects, and I must say, his talent and work ethic truly stood out. Recently, we were both part of the dynamic team at JMM Technologies, where Cekdar's professionalism and attention to detail shone brightly. I wholeheartedly endorse Cekdar for any opportunity that calls for a highly skilled and dedicated individual.",
     linkednURL: "/domain-expired"
   },
   {
     id: 2,
     name: 'Ahmet Can Bulut',
-    image: "images/nasir.jpg",
+    image: "images/muhammad.jpeg",
     designation: 'React JS | JavaScript | Tailwind | Sass | Bootstrap',
-    view: "I wholeheartedly recommend Osama as a talented React frontend developer with an incredible flair for UI/UX design. His proficiency in Node.js further enhances his capabilities, allowing him to build robust and scalable applications. Osama's attention to detail, problem-solving skills, and dedication to delivering exceptional results make him a valuable addition to any development team.",
+    view: "I wholeheartedly recommend Cekdar as a talented React frontend developer with an incredible flair for UI/UX design. His proficiency in Node.js further enhances his capabilities, allowing him to build robust and scalable applications. Cekdar's attention to detail, problem-solving skills, and dedication to delivering exceptional results make him a valuable addition to any development team.",
     linkednURL: "/domain-expired"
   },
   {
     id: 3,
     name: 'Muhammad Ullah',
-    image: "images/muhammad.jpeg",
+    image: "images/nasir.jpg",
     designation: 'Full Stack Developer',
-    view: "I highly recommend Osama Javed for web frontend development positions. Their expertise in ReactJS and Next.js, combined with their professionalism and dedication, make them an invaluable asset to any team.",
+    view: "I highly recommend Cekdar Javed for web frontend development positions. Their expertise in ReactJS and Next.js, combined with their professionalism and dedication, make them an invaluable asset to any team.",
     linkednURL: "/domain-expired"
   },
 ]
